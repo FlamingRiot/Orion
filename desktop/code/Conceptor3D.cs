2316bda753f60ad482d796fc67c6f197466fcd4a
Evan Comtesse <evan.comtesse@rpn.ch>
2025-01-27 12:06:52 +0100

    feat: added point detection on earth sphere

--- desktop/code/Conceptor3D.cs
+++ desktop/code/Conceptor3D.cs
@@ -7,7 +7,13 @@ namespace Orion_Desktop
     /// <summary>Represents the 3D conceptor of the program.</summary>
     internal static class Conceptor3D
     {
+        internal const int SKY_HEIGHT = 10;
+
+        internal static Ray MouseRay;
+        internal static RayCollision MouseRayCollision;
+
         internal static Camera3D Camera;
+        internal static Vector3 ISSPoint;
 
         /// <summary>Initializes the 3D conceptor.</summary>
         internal static void Init()
@@ -15,12 +21,17 @@ namespace Orion_Desktop
             // Create camera object.
             Camera = new Camera3D()
             {
-                Position = new Vector3(20.0f, 10.0f, 20.0f),
+                Position = new Vector3(25.0f, 15.0f, 25.0f),
                 Target = Vector3.Zero,
                 Up = Vector3.UnitY,
                 FovY = 45f,
                 Projection = CameraProjection.Perspective
             };
+
+            ISSPoint = new Vector3(1 * SKY_HEIGHT, 0, 0);
+
+            MouseRay = new Ray();
+            MouseRayCollision = new RayCollision();
         }
 
         /// <summary>Draws the components of the 3D conceptor to an opened render buffer.</summary>
@@ -28,9 +39,14 @@ namespace Orion_Desktop
         {
             BeginMode3D(Camera);
 
-            DrawGrid(5, 5);
+            // Draw sky box
+            DrawSphereWires(Vector3.Zero, SKY_HEIGHT, 20, 20, Color.Gray);
 
-            DrawLine3D(Vector3.Zero, new Vector3(5, 5, 5), Color.Red);
+            // Draw line
+            DrawLine3D(Vector3.Zero, ISSPoint, Color.Red);
+
+            // Draw ISS point
+            DrawSphere(ISSPoint, 0.2f, Color.Yellow);
 
             EndMode3D();
         }
@@ -38,7 +54,20 @@ namespace Orion_Desktop
         /// <summary>Updates the 3D conceptor.</summary>
         internal static void Update()
         {
+            // Update environment camera
             UpdateCamera(ref Camera, CameraMode.Orbital);
+
+            // Update point
+            UpdatePoint();
+        }
+
+        /// <summary>Updates the position of the ISS point on the sphere</summary>
+        private static void UpdatePoint()
+        {
+            MouseRay = GetScreenToWorldRay(GetMousePosition(), Camera);
+            MouseRayCollision = GetRayCollisionSphere(MouseRay, Vector3.Zero, SKY_HEIGHT);
+
+            ISSPoint = MouseRayCollision.Point;
         }
     }
 }

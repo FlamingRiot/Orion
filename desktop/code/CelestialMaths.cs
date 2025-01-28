ce6e34b9cddfbe4858d2b52922786a1eb4d029f3
Evan Comtesse <evan.comtesse@rpn.ch>
2025-01-28 08:54:51 +0100

    feat: extended satellite class applications

--- desktop/code/CelestialMaths.cs
+++ desktop/code/CelestialMaths.cs
@@ -3,15 +3,41 @@ using Raylib_cs;
 
 namespace Orion_Desktop
 {
+    /// <summary>Defines the visibility of a satellite.</summary>
+    internal enum SatelliteVisibility
+    {
+        daylight,
+        nighttime
+    }
+
+    /// <summary>Defines the different units of satellite velocity measurement.</summary>
+    internal enum SatelliteUnits
+    {
+        miles,
+        kilometers
+    }
+
+    /// <summary>Represents an instance of a <see cref="Satellite"/> object.</summary>
     internal struct Satellite
     {
+        // API informations
+        public string Name;
         public float Latitude;
         public float Longitude;
+        public float Altitude;
+        public float Velocity;
+        public SatelliteVisibility Visibility;
+        public float Footprint;
+        public long Timestamp;
+        public SatelliteUnits Units;
+
+        // Program informations
         public Vector3 RelativePosition;
 
         public Satellite() { }
     }
 
+    /// <summary>Represents the static class for celestial maths.</summary>
     internal class CelestialMaths
     {
         public const float EARTH_RADIUS = 6371; // Km
@@ -19,7 +45,7 @@ namespace Orion_Desktop
 
         internal static Vector3 ComputeECEF(float latitude, float longitude)
         {
-            // Fix negative latitude
+            // Fix negative/positive latitude
             if (longitude < 0) longitude = 180 + Math.Abs(longitude);
             else longitude = 180 - longitude;
             // Convert data to radians
--- desktop/code/OnlineRequests.cs
+++ desktop/code/OnlineRequests.cs
@@ -1,6 +1,7 @@
 ﻿#pragma warning disable CS8600
 #pragma warning disable CS8602
 #pragma warning disable CS8604
+#pragma warning disable CS8601
 
 using Newtonsoft.Json.Linq;
 
@@ -12,7 +13,7 @@ namespace Orion_Desktop
         {
             using (HttpClient client = new HttpClient()) 
             {
-                string url = "http://api.open-notify.org/iss-now.json";
+                string url = "https://api.wheretheiss.at/v1/satellites/25544";
                 // Try fetching, abort otherwise and go offline mode
                 try
                 {
@@ -23,19 +24,38 @@ namespace Orion_Desktop
                     string body = await response.Content.ReadAsStringAsync();
                     JObject json = JObject.Parse(body);
 
-                    // Extract data
-                    long timestamp = (long)json["timestamp"];
-                    float latitude = (float)json["iss_position"]["latitude"];
-                    float longitude = (float)json["iss_position"]["longitude"];
+                    // JSON Template from API
+                    //{
+                    //    "name": "iss",
+                    //    "id": 25544,
+                    //    "latitude": 50.11496269845,
+                    //    "longitude": 118.07900427317,
+                    //    "altitude": 408.05526028199,
+                    //    "velocity": 27635.971970874,
+                    //    "visibility": "daylight",
+                    //    "footprint": 4446.1877699772,
+                    //    "timestamp": 1364069476,
+                    //    "daynum": 2456375.3411574,
+                    //    "solar_lat": 1.3327003598631,
+                    //    "solar_lon": 238.78610691196,
+                    //    "units": "kilometers"
+                    //}
 
+                    // Extract data to satellite object
                     Satellite satellite = new Satellite()
                     {
-                        Latitude = latitude,
-                        Longitude = longitude,
+                        Name = (string)json["name"],
+                        Latitude = (float)json["latitude"],
+                        Longitude = (float)json["longitude"],
+                        Altitude = (float)json["altitude"],
+                        Velocity = (float)json["velocity"],
+                        Visibility = (SatelliteVisibility)Enum.Parse(typeof(SatelliteVisibility), (string)json["visibility"]),
+                        Units = (SatelliteUnits)Enum.Parse(typeof(SatelliteUnits), (string)json["units"]),
+                        Footprint = (float)json["footprint"],
+                        Timestamp = (long)json["timestamp"]
                     };
 
-                    Console.WriteLine($"Timestamp: {timestamp}");
-                    Console.WriteLine($"Latitude: {satellite.Latitude}, Longitude: {satellite.Longitude}");
+                    Console.WriteLine($"Latitude: {satellite.Latitude}, Longitude: {satellite.Longitude}, Altitude: {satellite.Altitude}");
 
                     return satellite;
                 }

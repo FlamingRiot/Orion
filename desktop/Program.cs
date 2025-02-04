8b803925296f70e8c5a27836823730ff94a6a9ed
Evan Comtesse <evan.comtesse@rpn.ch>
2025-02-04 08:31:39 +0100

    refactor: moved 3D rendering to render texture instead of screen

--- desktop/Program.cs
+++ desktop/Program.cs
@@ -1,4 +1,5 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 using static Raylib_cs.Raylib;
 
 namespace Orion_Desktop
@@ -8,7 +9,11 @@ namespace Orion_Desktop
     {
         // Constants
         public const string APP_NAME = "Orion";
-        public const string APP_VERSION = "0.1.0-alpha";  
+        public const string APP_VERSION = "0.1.0-alpha";
+
+        internal static RenderTexture2D Render;
+        internal static Rectangle SourceRender;
+        internal static Rectangle DestinationRender;
 
         /// <summary>Defines the entry point of the program.</summary>
         /// <param name="args">Arguments passed from outside the progam.</param>
@@ -22,6 +27,9 @@ namespace Orion_Desktop
             // Open different services
             Conceptor3D.Init();
 
+            // Load render texture
+            LoadRender();
+
             // Program loop
             //SetTargetFPS(60);
             DisableCursor();
@@ -30,14 +38,23 @@ namespace Orion_Desktop
                 // Update functions
                 Conceptor3D.Update();
                 
-                // Open drawing context
-                BeginDrawing();
+                // Start rendering to texture
+                BeginTextureMode(Render);
                 
                 // Define default background color
                 ClearBackground(Color.White);
 
                 // Draw functions
                 Conceptor3D.Draw();
+
+                // Close rendering to texture
+                EndTextureMode();
+
+                // Begin screen rendering
+                BeginDrawing();
+                
+                // Draw Render Texture
+                DrawTexturePro(Render.Texture, SourceRender, DestinationRender, Vector2.Zero, 0, Color.White);
 
                 // Draw debug framerate
                 DrawFPS(10, 10);    
@@ -48,6 +65,17 @@ namespace Orion_Desktop
 
             // Close window and unload raylib default resources
             CloseWindow();
+        }
+
+        /// <summary>Loads the <see cref="RenderTexture2D"/> for post-processing effects.</summary>
+        internal static void LoadRender()
+        {
+            int width = GetScreenWidth();
+            int height = GetScreenHeight(); 
+
+            Render = LoadRenderTexture(width, height);
+            SourceRender = new Rectangle(0, 0, width, -height);
+            DestinationRender = new Rectangle(0, 0, width, height);
         }
     }
 }
